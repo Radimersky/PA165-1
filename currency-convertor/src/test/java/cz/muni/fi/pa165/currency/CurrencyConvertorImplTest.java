541cf7d6a681ef570fa225ce0922f88a4fe200e1
Marek Radiměřský <xradimer@fi.muni.cz>
2020-02-24 19:57:50 +0100

    castecne vytvorene testy

--- currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
+++ currency-convertor/src/test/java/cz/muni/fi/pa165/currency/CurrencyConvertorImplTest.java
@@ -1,14 +1,62 @@
 package cz.muni.fi.pa165.currency;
 
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CurrencyConvertorImplTest {
 
+    private static final Currency CZK = Currency.getInstance("CZK");
+    private static final Currency EUR = Currency.getInstance("EUR");
+
+    @Mock
+    private ExchangeRateTable exchangeRateTable;
+
+    private CurrencyConvertorImpl currencyConvertor;
+
+    @Before
+    public void createCurrencyConvertor(){
+        this.currencyConvertor = new CurrencyConvertorImpl(exchangeRateTable);
+    }
+
     @Test
-    public void testConvert() {
-        // Don't forget to test border values and proper rounding.
-        fail("Test is not implemented yet.");
+    public void assertionsDemonstration() throws ExternalServiceFailureException {
+
+        when(exchangeRateTable.getExchangeRate(CZK,EUR)).thenReturn(new BigDecimal("0.01"));
+
+        BigDecimal actualResult = currencyConvertor.convert(CZK, EUR, new BigDecimal("1.49"));
+        BigDecimal expectedResult = new BigDecimal("0.01");
+
+        //assertTrue(actualResult.equals(expectedResult));
+
+        //assertEquals(expectedResult, actualResult);
+
+        // Hamcrest way
+        //assertThat(actualResult, equalTo(expectedResult));
+
+        // AssertJ way
+        assertThat(actualResult).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void testConvert() throws ExternalServiceFailureException {
+        when(exchangeRateTable.getExchangeRate(CZK,EUR)).thenReturn(new BigDecimal("0.01"));
+
+        assertThat(currencyConvertor.convert(CZK,EUR,new BigDecimal("1.49")))
+                .isEqualTo("0.01");
+        //???
     }
 
     @Test
@@ -32,8 +80,15 @@ public class CurrencyConvertorImplTest {
     }
 
     @Test
-    public void testConvertWithExternalServiceFailure() {
-        fail("Test is not implemented yet.");
+    public void testConvertWithExternalServiceFailure() throws ExternalServiceFailureException {
+
+        ExternalServiceFailureException exception = new ExternalServiceFailureException("Exchange rate lookup failed for source currency CZK and target currency EUR");
+        when(exchangeRateTable.getExchangeRate(CZK,EUR)).thenThrow(exception);
+
+        assertThatExceptionOfType(UnknownExchangeRateException.class)
+                .isThrownBy(() -> currencyConvertor.convert(CZK,EUR,BigDecimal.ONE))
+                .withCause(exception)
+                .withMessage("Exchange rate lookup failed for source currency CZK and target currency EUR");
     }
 
 }
